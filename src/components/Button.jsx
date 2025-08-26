15ceb16960e03a3561a17e8987523f7a34b88b3e
Emmanuel Oduntan <oduntane@icloud.com>
2025-08-26 08:56:44 +0100

    chore: add dark theme

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,9 @@ export default function Button({type, children, className}) {
     let buttonStyle = ''
 
     switch(type) {
-        case 'primary': buttonStyle = 'text-3 bg-gray-200 text-gray-900 shadow-[inset_0_-4px_0_0_#A79E91] hover:bg-white active:bg-white'; break;
-        case 'secondary': buttonStyle = 'text-5 bg-blue-500 text-white shadow-[inset_0_-4px_0_0_#1B6066] hover:bg-blue-400 active:bg-blue-400'; break;
-        case 'tertiary': buttonStyle = 'text-5 bg-orange-700 text-white shadow-[inset_0_-4px_0_0_#873901] hover:bg-orange-400 active:bg-orange-400'; break;
+        case 'primary': buttonStyle = 'text-3 bg-gray-200 text-gray-900 shadow-[inset_0_-4px_0_0_#A79E91] hover:bg-white active:bg-white dark:text-navy-750'; break;
+        case 'secondary': buttonStyle = 'text-5 bg-blue-500 text-white shadow-[inset_0_-4px_0_0_#1B6066] hover:bg-blue-400 active:bg-blue-400 dark:bg-navy-700 dark:shadow-[inset_0_-4px_0_0_#414E73] dark:hover:bg-navy-400 dark:active:bg-navy-400'; break;
+        case 'tertiary': buttonStyle = 'text-5 bg-orange-700 text-white shadow-[inset_0_-4px_0_0_#873901] hover:bg-orange-400 active:bg-orange-400 dark:bg-red-600 dark:shadow-[inset_0_-4px_0_#93261A] dark:hover:bg-red-400 dark:active:bg-red-400'; break;
 
     }
 
--- src/components/ThemeToggleButton.jsx
+++ src/components/ThemeToggleButton.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 function ThemeToggleButton() {
 
-    const [theme, setTheme] = useState('light')
+    const [theme, setTheme] = useState('dark')
 
     useEffect(() => {
         if (theme === 'light') {
@@ -14,7 +14,7 @@ function ThemeToggleButton() {
         }
     }, [theme])
 
-    const position = theme === 'light' ? 'justify-start' : theme === 'dark' ? 'justify-center' : 'justify-end'
+    const position = theme === 'dark' ? 'justify-start' : theme === 'light' ? 'justify-center' : 'justify-end'
 
 
     function handleToggleEvent(event) {
@@ -45,9 +45,9 @@ function ThemeToggleButton() {
         let position = offset / width
 
         if (position < (1/3)) {
-            setTheme('light')
-        } else if (position < (2/3) && position > (1/3)) {
             setTheme('dark')
+        } else if (position < (2/3) && position > (1/3)) {
+            setTheme('light')
         } else {
             setTheme('custom')
         }
@@ -57,13 +57,13 @@ function ThemeToggleButton() {
     
     return (
         <div className="w-[4.25rem] flex flex-col gap-2">
-            <div className="flex justify-around text-6 text-gray-900">
+            <div className="flex justify-around text-6 text-gray-900 dark:text-white">
                 <span>1</span>
                 <span>2</span>
                 <span>3</span>
             </div>
-            <div className={`w-full h-[1.625rem] bg-gray-300 rounded-[0.8125rem] p-[0.3125rem] flex ${position}`} data-toggle-container onClick={handleToggleEvent}>
-                <div tabIndex="0" className="w-4 aspect-square cursor-pointer bg-orange-700 rounded-full hover:bg-orange-400 active:bg-orange-400">
+            <div className={`w-full h-[1.625rem] bg-gray-300 rounded-[0.8125rem] p-[0.3125rem] flex ${position} dark:bg-navy-900`} data-toggle-container onClick={handleToggleEvent}>
+                <div tabIndex="0" className="w-4 aspect-square cursor-pointer bg-orange-700 rounded-full hover:bg-orange-400 active:bg-orange-400 dark:bg-red-600 dark:hover:bg-red-400 dark:active:bg-red-400">
 
                 </div>
             </div>
